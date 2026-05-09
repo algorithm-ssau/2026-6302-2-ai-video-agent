c7170d631fb99f65cb772ec46a19e02a3c34369e
soloda1 <2023-01986@students.ssau.ru>
2026-05-09 14:01:35 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["viaduct.proxy.rlwy.net"],
   serverExternalPackages: ["@remotion/bundler", "@remotion/renderer", "esbuild", "@esbuild/win32-x64"],
 };
 
