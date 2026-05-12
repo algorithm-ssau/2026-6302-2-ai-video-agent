19909fff9a08822df97e4c5d359146fe67e6de7b
Ariel-cano <vaskaivanov856@gmail.com>
2026-05-12 22:07:37 +0400

    fix: fix schedule to UTC+4 time part2

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest-client";
-import { helloWorld, generateVideo } from "@/lib/inngest";
+import { helloWorld, generateVideo, publishScheduledSeries } from "@/lib/inngest";
 
 export const dynamic = "force-dynamic";
 
 const handler = serve({
   client: inngest,
-  functions: [helloWorld, generateVideo],
+  functions: [helloWorld, generateVideo, publishScheduledSeries],
 });
 
 export { handler as GET, handler as POST, handler as PUT };
